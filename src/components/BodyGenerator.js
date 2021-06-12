027c76c98545a0116f49b8dfdc83163ebfe40f8b
Texmo100 <isaac.ir@hotmail.com>
2021-06-11 18:20:06 -0700

    almost finish, y-MemeGenerator alpha version 1.0

--- src/components/BodyGenerator.js
+++ src/components/BodyGenerator.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const BodyGenerator = () => {
-    return(
-        <div className="body-app">
-            <div className="m-generator-container"></div>
-        </div>
-    )
+class BodyGenerator extends Component {
+    constructor() {
+        super()
+        this.state = {
+            topText: '',
+            bottomText: ''
+        }
+
+        this.textHandle = this.textHandle.bind(this)
+    }
+
+    textHandle = (event) => {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+    }
+
+    render() {
+        return (
+            <div className="body-app">
+                <div className="m-generator-container">
+                    {/* Meme View Image */}
+                    <image className="meme-view" src='../images/image-01.jpg' alt="meme"/>
+
+                    {/* Meme Form */}
+                    <div className="meme-form">
+                        <div className="input-container">
+                            <label>Top Text</label>
+                            <input
+                                type="text"
+                                name="topText"
+                                value={this.topText}
+                                placeholder="top text"
+                                onChange={this.textHandle}
+                            />
+                        </div>
+
+                        <div className="input-container">
+                            <label>Top Text</label>
+                            <input
+                                type="text"
+                                name="bottomText"
+                                value={this.bottomText}
+                                placeholder="bottom text"
+                                onChange={this.textHandle}
+                            />
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default BodyGenerator
